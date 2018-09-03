331478a1a516300ec812606350cbe784691f3e6e
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-09-02 21:02:40 -0700

    ✨ Cleaning

---
+++ Relax Unity Files/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/* Author: Andrew De Leon
+ * */
+
+public class MenuManager : MonoBehaviour {
+    
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
---
+++ Relax Unity Files/Assets/Scripts/MeteorManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Author: Andrew De Leon
+ * */
+
+public class MeteorManager : MonoBehaviour {
+
+    public int range = 50;
+
+	void Start ()
+    {
+        InvokeRepeating("createMeteors", 0.0f, 4.0f);
+    }
+
+    // Creates meteors high on the y-axis, far from the character (z-axis), at random x-axis spots still visible
+    void createMeteors()
+    {
+        int randomNumber = Random.Range(-60, 120);
+
+        for (int y = 50; y < 55; y++)
+        {
+            for (int x = randomNumber; x < randomNumber + 5; x++)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.AddComponent<Rigidbody>();
+                cube.transform.position = new Vector3(x, y, range);
+                // Destroy cubes after 10 seconds to avoid memory overlord
+                Destroy(cube, 10);
+            }
+        }
+    }
+}
