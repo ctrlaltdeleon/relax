8c0663720f779263631821265706432fb1b624b5
AC De Leon <acfromspace.deleon@gmail.com>
2019-12-31 10:43:43 -0800

    [*] PauseManager.cs 🌱

--- Development Area/Assets/Scripts/PauseManager.cs
+++ Development Area/Assets/Scripts/PauseManager.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/* Author: Andrew De Leon
- * */
-
 public class PauseManager : MonoBehaviour {
 
     public static bool GameIsPaused = false;
